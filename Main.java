b1002dbb11c650f75f00e48512ebba497586a5a9
TarnishedGhost <biliy.bohdan@lll.kpi.ua>
2025-06-08 00:34:37 +0300

    Update main

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
+import commands.Command;
 import controller.SmartHomeController;
 import devices.Light;
 import devices.AirConditioner;
 import devices.Alarm;
+import commands.TurnOnCommand;
+import commands.TurnOffCommand;
+import devices.SmartDevice;
+import remote.RemoteControl;
+
+
 
 public class Main {
     public static void main(String[] args) {
         SmartHomeController controller = SmartHomeController.getInstance();
 
+        SmartDevice light = new Light("Living Room");
+        SmartDevice air = new AirConditioner("Bedroom");
+        SmartDevice alarm = new Alarm("Security");
+
         controller.addDevice(new Light("Living Room"));
         controller.addDevice(new AirConditioner("Bedroom"));
         controller.addDevice(new Alarm("Security"));
 
-        controller.turnOnAll();      
-        controller.turnOff("Bedroom");
-        controller.turnOn("Security"); 
-        controller.turnOffAll(); 
+        Command turnOnLight = new TurnOnCommand(light);
+        Command turnOffAC = new TurnOffCommand(air);
+        Command turnOnAlarm = new TurnOnCommand(alarm);
+
+        RemoteControl remote = new RemoteControl();
+        remote.addCommand(turnOnLight);
+        remote.addCommand(turnOffAC);
+        remote.addCommand(turnOnAlarm);
+
+        remote.executeAll();
     }
 }
